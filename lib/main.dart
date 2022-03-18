5bfab235a364a4c418bac9a5bf81ae08c6f41974
joseph81301 <joseph81301@gmail.com>
2022-03-18 16:16:25 -0400

    hi

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,184 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const DayToDay());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class DayToDay extends StatelessWidget {
+  const DayToDay({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: "DayToDay",
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        brightness: Brightness.light,
+        /* light theme settings */
+
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        /* dark theme settings */
+        scaffoldBackgroundColor: Colors.black,
+
+      ),
+      themeMode: ThemeMode.system,
+      /* ThemeMode.system to follow system theme,
+         ThemeMode.light for light theme,
+         ThemeMode.dark for dark theme
+      */
+      home: const MyStatefulWidget(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+/// AnimationControllers can be created with `vsync: this` because of TickerProviderStateMixin.
+class _MyStatefulWidgetState extends State<MyStatefulWidget>
+    with TickerProviderStateMixin{
+  late TabController _tabController;
 
   @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 4, vsync: this);
+  }
+
+  @mustCallSuper
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    var systemColor = MediaQuery.of(context).platformBrightness;
+    bool darkMode = systemColor == Brightness.dark;
+    Color labelColorChange;
+
+    if (darkMode) {
+      labelColorChange = Colors.blueAccent;
+    }
+    else {
+      labelColorChange = Colors.deepOrangeAccent;
+    }
+
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        title: const Text('DayToDay'),
+        //backgroundColor: Colors.black,
+        bottom: TabBar(
+          indicatorColor: Colors.blue,
+          labelColor: labelColorChange,
+          controller: _tabController,
+          indicatorSize: TabBarIndicatorSize.label,
+          tabs: <Widget>[
+            Tab(
+              text: "Calendar",
+
+              icon: Image.asset('icons/icons8-calendar-48.png', scale: 2,),
+
+              //icon: ImageIcon(
+              //  AssetImage("assets/icons/icons8-calendar-48.png"),
+              //  size: 24,
+              //),
+
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            const Tab(
+              text: "To-do",
+            ),
+            const Tab(
+              text: "Projects",
+            ),
+            const Tab(
+              text: "Homework",
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          Center(
+            child: calendarWidget(),
+
+          ),
+          Center(
+            child: Text("To do"),
+          ),
+          Center(
+            child: Text("Projects"),
+          ),
+          Center(
+            child: Text("HW"),
+          )
+        ],
+      ),
+    );
+  }
+
+  //Keeps state of each tab, in other words doesnt reset tab once you change tabs, i think
+
+  Column calendarWidget() {
+    DateTime f = DateTime.now();
+    var months = {
+      1:"January",
+      2:"February",
+      3:"March",
+      4:"April",
+      5:"May",
+      6:"June",
+      7:"July",
+      8:"August",
+      9:"September",
+      10:"October",
+      11:"November",
+      12:"December"};
+    var days = {
+      1:"Sun",
+      2:"Mon",
+      3:"Tues",
+      4:"Wed",
+      5:"Thu",
+      6:"Fri",
+      7:"Sat"
+    };
+    List<Widget> ex = [];
+
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text("Feb"),
+        Container(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                child: Text("h"),
+                padding: EdgeInsets.all(5),
+              ),
+              Container(
+                child: Text("attemp"),
+                padding: EdgeInsets.all(5),
+              )
+            ],
+          ),
+          padding: EdgeInsets.only(top: 30),
+        ),
+        Container(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                child: Text("attemp"),
+                padding: EdgeInsets.all(5),
+              )
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
